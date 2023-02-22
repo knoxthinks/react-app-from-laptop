e479a968d1a32bf89f5b2f3d7a7c678e85e75326
knoxLaptop <itsknox@gmail.com>
2023-02-22 14:44:25 -0500

    staging

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          WAN Group
         </a>
         {/* <ButtonBases /> */}
         <SpeedDialTooltipOpen />
